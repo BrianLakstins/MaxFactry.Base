e01bc8359a13a0efe3d70fadb5610199477cee59
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2022-11-07 20:37:42 -0500

    Update NuGet package

--- .nuget/package.bat
+++ .nuget/package.bat
@@ -1,6 +1,10 @@
 rem Package the library for Nuget
 copy ..\MaxFactry.Base-NF-2.0\bin\Release\MaxFactry.Base*.dll lib\net20\
 copy ..\MaxFactry.Base-NF-4.5.2\bin\Release\MaxFactry.Base*.dll lib\net452\
+copy ..\MaxFactry.Base-NF-4.7.2\bin\Release\MaxFactry.Base*.dll lib\net472\
+copy ..\MaxFactry.Base-NF-4.8\bin\Release\MaxFactry.Base*.dll lib\net48\
 copy ..\MaxFactry.Base-NC-2.1\bin\Release\netcoreapp2.1\MaxFactry.Base*.dll lib\netcoreapp2.1\
+copy ..\MaxFactry.Base-NC-3.1\bin\Release\netcoreapp3.1\MaxFactry.Base*.dll lib\netcoreapp3.1\
+copy ..\MaxFactry.Base-NC-6.0\bin\Release\net6.0\MaxFactry.Base*.dll lib\net6.0\
 
 c:\install\nuget\nuget.exe pack MaxFactry.Base.nuspec -OutputDirectory "packages" -IncludeReferencedProjects -properties Configuration=Release 
