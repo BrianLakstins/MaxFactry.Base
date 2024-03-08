99039d8ab68e11ab559d8df7cc1c971a3ff1096e
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2024-03-08 15:45:00 -0500

    Add .NET Core 8 project Update NuGet package

--- .nuget/package.bat
+++ .nuget/package.bat
@@ -6,5 +6,6 @@ copy ..\MaxFactry.Base-NF-4.8\bin\Release\MaxFactry.Base*.dll lib\net48\
 copy ..\MaxFactry.Base-NC-2.1\bin\Release\netcoreapp2.1\MaxFactry.Base*.dll lib\netcoreapp2.1\
 copy ..\MaxFactry.Base-NC-3.1\bin\Release\netcoreapp3.1\MaxFactry.Base*.dll lib\netcoreapp3.1\
 copy ..\MaxFactry.Base-NC-6.0\bin\Release\net6.0\MaxFactry.Base*.dll lib\net6.0\
+copy ..\MaxFactry.Base-NC-8.0\bin\Release\net8.0\MaxFactry.Base*.dll lib\net8.0\
 
 c:\install\nuget\nuget.exe pack MaxFactry.Base.nuspec -OutputDirectory "packages" -IncludeReferencedProjects -properties Configuration=Release 
